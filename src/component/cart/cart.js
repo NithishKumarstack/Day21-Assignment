aba9757e118eb7bb6090b4ce096a0e6c44db61c9
NithishKumarstack <officialnithish13@gmail.com>
2023-12-11 18:36:13 +0530

    functionality

--- src/component/cart/cart.js
+++ src/component/cart/cart.js
@@ -2,8 +2,8 @@ import './cart.css'
 const Cart = (props) => {
     const {list, cartItems, addToCart} = props;
     const addingItemToCart = (item) => {
-      addToCart({...item, quantity: 1})
-    }
+      addToCart({...item, quantity: 1});
+    };
     return (
        <div className='cart'>
          <div>
@@ -12,7 +12,7 @@ const Cart = (props) => {
         <div className='info'>{list.quantity < 50 ? (<span className='few'>Only Few Items</span>):(<span className='avail'>Available</span>)}</div>
         <h4>Product Name:{list.name}</h4>
         <h4>Price:{list.price}</h4>
-        <div>{cartItems.some((item) => item.id === list.id) ? (<button className='remove' onClick={addingItemToCart(list)}>Remove From Cart</button>) : (<button className='add' onClick={addingItemToCart(list)}>Add To Cart</button>) }</div>
+        <div>{cartItems.some((item) => item.id === list.id) ? (<button className='remove' >Remove From Cart</button>) : (<button className='add' onClick={() => addingItemToCart(list)}>Add To Cart</button>) }</div>
        </div>
     );
 };
